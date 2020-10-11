8cbe6312caf6ca936d2bb11259f588c9dc499bd3
fwend <josvanuden@gmail.com>
2020-10-11 17:01:24 +0200

    add category form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\ChallengeCategory;
+use App\Form\CategoryType;
+use App\Repository\ChallengeCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @Route("/categories/{section}", name="list_categories")
+     * @param Request $request
+     * @param ChallengeCategory $category
+     * @param ChallengeCategoryRepository $repo
+     * @return Response
      */
-    public function index()
+    public function index(Request $request, ChallengeCategory $category, ChallengeCategoryRepository $repo)
     {
+        $categories = $repo->findBy(['section' => $category]);
+
+        $form = $this->createForm(CategoryType::class, $category = new ChallengeCategory());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('list_categories');
+        }
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
+            'form' => $form->createView()
         ]);
     }
 }

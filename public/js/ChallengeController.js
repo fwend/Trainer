cab5cff8af5acd0021b251d115d50943256b49f3
fwend <josvanuden@gmail.com>
2020-12-27 23:06:18 +0100

    use macro for custom collection type

--- public/js/ChallengeController.js
+++ public/js/ChallengeController.js
@@ -5,12 +5,6 @@ class ChallengeController {
     }
 
     handleAnswers() {
-        const deleteButtonTemplate = `<div data-delete-target class="answer row mt-1">
-            <div class="col-11">__content__</div>
-            <div class="col-1">
-            <button data-delete-button class="btn btn-secondary" type="button">x</button></div>
-            </div>`;
-
         const container = document.querySelector('#challenge_answers');
         const answers = container.querySelectorAll('div.answer');
         const addButton = document.getElementById("challenge_add_answer");
@@ -31,7 +25,7 @@ class ChallengeController {
             const prototype = container.dataset.prototype;
             const content = prototype.replace(/__name__/g, index);
             const newControl = document.createElement('div');
-            newControl.innerHTML = deleteButtonTemplate.replace(/__content__/, content);
+            newControl.innerHTML = content;
             container.appendChild(newControl);
 
             container.dataset.index = (index + 1).toString();

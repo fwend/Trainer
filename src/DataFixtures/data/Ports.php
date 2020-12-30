12cf7a863b621b8e09d6c5e7534b048ab997521a
fwend <josvanuden@gmail.com>
2020-12-30 02:38:09 +0100

    update ports fixtures

--- src/DataFixtures/data/Ports.php
+++ src/DataFixtures/data/Ports.php
@@ -54,10 +54,154 @@ class Ports
                 'link' => 'https://en.wikipedia.org/wiki/SSH_(Secure_Shell)'
             ],
             [
-                'name' => '	Simple Mail Transfer Protocol (SMTP)',
+                'name' => 'Simple Mail Transfer Protocol (SMTP)',
                 'content' => 'Simple Mail Transfer Protocol',
                 'answers' => [
                     '25',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'WHOIS protocol',
+                'content' => 'WHOIS protocol',
+                'answers' => [
+                    '43',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Domain Name System (DNS)',
+                'content' => 'Domain Name System',
+                'answers' => [
+                    '53',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Hypertext Transfer Protocol (HTTP)',
+                'content' => 'Hypertext Transfer Protocol',
+                'answers' => [
+                    '80',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Network News Transfer Protocol (NNTP)',
+                'content' => 'Network News Transfer Protocol',
+                'answers' => [
+                    '119',
+                ],
+                'note' => 'Usenet',
+                'link' => ''
+            ],
+            [
+                'name' => 'Network Time Protocol (NTP)',
+                'content' => 'Network Time Protocol',
+                'answers' => [
+                    '123',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Internet Relay Chat (IRC)',
+                'content' => 'Internet Relay Chat',
+                'answers' => [
+                    '194',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Hypertext Transfer Protocol Secure (HTTPS)',
+                'content' => 'Hypertext Transfer Protocol Secure',
+                'answers' => [
+                    '443',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Remote procedure call (RPC)',
+                'content' => 'Remote procedure call',
+                'answers' => [
+                    '530',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'DOOM',
+                'content' => 'DOOM',
+                'answers' => [
+                    '666',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Internet Message Access Protocol Secure (IMAPS)',
+                'content' => 'Internet Message Access Protocol Secure',
+                'answers' => [
+                    '993',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'FTPS Protocol (data), FTP Secure',
+                'content' => 'FTPS Protocol (data)',
+                'answers' => [
+                    '989',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'FTPS Protocol (control), FTP Secure',
+                'content' => 'FTPS Protocol (control), FTP Secure',
+                'answers' => [
+                    '990',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Internet Relay Chat Secure (IRCS)',
+                'content' => 'Internet Relay Chat Secure',
+                'answers' => [
+                    '994',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Post Office Protocol 3 Secure (POP3S)',
+                'content' => 'Post Office Protocol 3 Secure',
+                'answers' => [
+                    '995',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'NNTP Secure (NNTPS)',
+                'content' => 'NNTP Secure',
+                'answers' => [
+                    '563',
+                ],
+                'note' => '',
+                'link' => ''
+            ],
+            [
+                'name' => 'Authenticated SMTP Secure (SMTPS)',
+                'content' => 'Authenticated SMTP Secure',
+                'answers' => [
+                    '465',
                 ],
                 'note' => '',
                 'link' => ''

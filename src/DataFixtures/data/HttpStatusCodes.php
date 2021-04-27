ffc4edb5a1f3361b7fde4ce8cebcdde1b0f655ba
fwend <josvanuden@gmail.com>
2021-04-27 11:08:04 +0200

    add mysql, update http status codes

--- src/DataFixtures/data/HttpStatusCodes.php
+++ src/DataFixtures/data/HttpStatusCodes.php
@@ -224,6 +224,222 @@ class HttpStatusCodes
                 'note' => 'the server was acting as a gateway or proxy and did not receive a timely response from the upstream server.',
                 'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
             ],
+            [
+                'name' => 'Informational: 100',
+                'content' => '100',
+                'answers' => [
+                    'Continue',
+                ],
+                'note' => 'the server has received the request headers and the client should proceed to send the request body (in the case of a request for which a body needs to be sent; for example, a POST request). Sending a large request body to a server after a request has been rejected for inappropriate headers would be inefficient. To have a server check the request\'s headers, a client must send Expect: 100-continue as a header in its initial request and receive a 100 Continue status code in response before sending the body. ',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Informational: 101',
+                'content' => '101',
+                'answers' => [
+                    'Switching Protocols',
+                ],
+                'note' => 'the requester has asked the server to switch protocols and the server has agreed to do so. ',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Success: 200',
+                'content' => '200',
+                'answers' => [
+                    'OK',
+                ],
+                'note' => 'Standard response for successful HTTP requests',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Success: 201',
+                'content' => '201',
+                'answers' => [
+                    'Created',
+                ],
+                'note' => 'the request has been fulfilled, resulting in the creation of a new resource.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Success: 202',
+                'content' => '202',
+                'answers' => [
+                    'Accepted',
+                ],
+                'note' => 'the request has been accepted for processing, but the processing has not been completed. The request might or might not be eventually acted upon, and may be disallowed when processing occurs.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Success: 204',
+                'content' => '204',
+                'answers' => [
+                    'Accepted',
+                ],
+                'note' => 'the server successfully processed the request, and is not returning any content.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Redirection: 301',
+                'content' => '301',
+                'answers' => [
+                    'Moved Permanently',
+                ],
+                'note' => 'this and all future requests should be directed to the given URI',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Redirection: 304',
+                'content' => '304',
+                'answers' => [
+                    'Not Modified',
+                ],
+                'note' => 'Indicates that the resource has not been modified since the version specified by the request headers If-Modified-Since or If-None-Match. ',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Redirection: 307',
+                'content' => '307',
+                'answers' => [
+                    'Temporary Redirect',
+                ],
+                'note' => '',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 400',
+                'content' => '400',
+                'answers' => [
+                    'Bad Request',
+                ],
+                'note' => 'the server cannot or will not process the request due to an apparent client error (e.g. malformed request syntax, size too large)',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 401',
+                'content' => '401',
+                'answers' => [
+                    'Unauthorized',
+                ],
+                'note' => '',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 403',
+                'content' => '403',
+                'answers' => [
+                    '403',
+                ],
+                'note' => 'the request contained valid data and was understood by the server, but the server is refusing action due to the user having insufficient permissions',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 404',
+                'content' => '404',
+                'answers' => [
+                    'Not Found',
+                ],
+                'note' => 'the requested resource could not be found.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 405',
+                'content' => '405',
+                'answers' => [
+                    'Method Not Allowed',
+                ],
+                'note' => 'a request method is not supported for the requested resource; for example, a GET request on a form that requires data to be presented via POST, or a PUT request on a read-only resource.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 406',
+                'content' => '406',
+                'answers' => [
+                    'Not Acceptable',
+                ],
+                'note' => 'the requested resource is capable of generating only content not acceptable according to the Accept headers sent in the request.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 408',
+                'content' => '408',
+                'answers' => [
+                    'Request Timeout',
+                ],
+                'note' => 'the server timed out waiting for the request.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 409',
+                'content' => '409',
+                'answers' => [
+                    'Conflict',
+                ],
+                'note' => 'Indicates that the request could not be processed because of conflict in the current state of the resource, such as an edit conflict between multiple simultaneous updates',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Client Error: 418',
+                'content' => '418',
+                'answers' => [
+                    "I'm a teapot",
+                ],
+                'note' => 'the RFC specifies this code should be returned by teapots requested to brew coffee. April Fools\' Joke',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => "Client Error: 451",
+                'content' => "451",
+                'answers' => [
+                    'Unavailable For Legal Reasons',
+                ],
+                'note' => 'a server operator has received a legal demand to deny access to a resource or to a set of resources that includes the requested resource. The code 451 was chosen as a reference to the novel Fahrenheit 451.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Server Error: 500',
+                'content' => '500',
+                'answers' => [
+                    'Internal Server Error',
+                ],
+                'note' => 'a generic error message, given when an unexpected condition was encountered and no more specific message is suitable.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Server Error: 501',
+                'content' => '501',
+                'answers' => [
+                    'Not Implemented',
+                ],
+                'note' => 'the server either does not recognize the request method, or it lacks the ability to fulfil the request. ',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Server Error: 502',
+                'content' => '502',
+                'answers' => [
+                    'Bad Gateway',
+                ],
+                'note' => 'the server was acting as a gateway or proxy and received an invalid response from the upstream server.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Server Error: 503',
+                'content' => '503',
+                'answers' => [
+                    'Service Unavailable',
+                ],
+                'note' => 'the server cannot handle the request (because it is overloaded or down for maintenance). Generally, this is a temporary state.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
+            [
+                'name' => 'Server Error: 504',
+                'content' => '504',
+                'answers' => [
+                    'Gateway Timeout',
+                ],
+                'note' => 'the server was acting as a gateway or proxy and did not receive a timely response from the upstream server.',
+                'link' => 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
+            ],
         ]
     ];
 }
--- src/DataFixtures/data/Mysql.php
+++ src/DataFixtures/data/Mysql.php
@@ -9,13 +9,184 @@ class Mysql
     public static array $data = [
         'general' => [
             [
-                'name' => 'Test',
-                'content' => 'Test',
+                'name' => 'Create database',
+                'content' => 'Create database "test"',
                 'answers' => [
-                    'test',
+                    'CREATE DATABASE test;',
                 ],
-                'note' => 'test',
-                'link' => 'test'
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-create-database/'
+            ],
+            [
+                'name' => 'List databases',
+                'content' => 'List all databases',
+                'answers' => [
+                    'SHOW DATABASES;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-show-databases/'
+            ],
+            [
+                'name' => 'Delete database',
+                'content' => 'Delete database "test"',
+                'answers' => [
+                    'DROP DATABASE test;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-drop-database/'
+            ],
+            [
+                'name' => 'Select database',
+                'content' => 'Select "test" database to work with',
+                'answers' => [
+                    'USE test;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-select-database/'
+            ],
+            [
+                'name' => 'Current database name',
+                'content' => 'Find out the name of the currently selected database',
+                'answers' => [
+                    'SELECT database();',
+                ],
+                'note' => '',
+                'link' => 'https://www.tutorialspoint.com/how-to-check-which-database-is-selected-in-mysql'
+            ],
+            [
+                'name' => 'Create database',
+                'content' => 'Create database "test"',
+                'answers' => [
+                    'CREATE DATABASE test;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-create-database/'
+            ],
+            [
+                'name' => 'Create table',
+                'content' => 'Create a testtable with a primary key id column',
+                'answers' => [
+                    'CREATE TABLE testtable (id INT AUTO_INCREMENT PRIMARY KEY);',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-create-table/'
+            ],
+            [
+                'name' => 'Column definition 1',
+                'content' => 'Create a string column "column1", variable length of 100, not null, default null',
+                'answers' => [
+                    'column1 VARCHAR(100) NOT NULL DEFAULT NULL',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-create-table/'
+            ],
+            [
+                'name' => 'Column definition 2',
+                'content' => 'Create a integer column "column1" that is unique',
+                'answers' => [
+                    'column1 INT NOT NULL UNIQUE',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-unique-constraint/'
+            ],
+            [
+                'name' => 'Unique',
+                'content' => 'Make column1 and column2 unique',
+                'answers' => [
+                    'UNIQUE(column1,column2)',
+                ],
+                'note' => 'Also: CONSTRAINT name UNIQUE (column1 , column2)',
+                'link' => 'https://www.mysqltutorial.org/mysql-unique-constraint/'
+            ],
+            [
+                'name' => 'Delete table',
+                'content' => 'Delete table "test"',
+                'answers' => [
+                    'DROP TABLE test;',
+                ],
+                'note' => 'Also DROP TABLE IF EXISTS test; DROP TABLE table1, table2',
+                'link' => 'https://www.mysqltutorial.org/mysql-create-database/'
+            ],
+            [
+                'name' => 'Add row to table',
+                'content' => 'Add a row to table "test", with column1 and column2',
+                'answers' => [
+                    'INSERT INTO test (column1, column2) VALUES(val1, val2);',
+                ],
+                'note' => 'Also: VALUES(val1, val2), (val3, val4) etc',
+                'link' => 'https://www.mysqltutorial.org/mysql-insert-statement.aspx'
+            ],
+            [
+                'name' => 'Update row 1',
+                'content' => 'Update column1 value to "val2" in table "test"...',
+                'answers' => [
+                    'UPDATE TABLE test SET column1 = val2',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-alter-table.aspx'
+            ],
+            [
+                'name' => 'Update row 2',
+                'content' => 'Update column1 value to column2 value in table "test"...',
+                'answers' => [
+                    'UPDATE TABLE test SET column1 = column2',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-alter-table.aspx'
+            ],
+            [
+                'name' => 'List warnings',
+                'content' => 'List warnings if any',
+                'answers' => [
+                    'SHOW WARNINGS;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-cheat-sheet.aspx'
+            ],
+            [
+                'name' => 'Change table 1',
+                'content' => 'Change table column "column1" of table "test", int nullable default 0',
+                'answers' => [
+                    'ALTER TABLE test modify column1 INT NULL DEFAULT 0;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-alter-table.aspx'
+            ],
+            [
+                'name' => 'Change table 2',
+                'content' => 'Change table column name "column1" of table "test" to column2',
+                'answers' => [
+                    'ALTER TABLE test CHANGE COLUMN column1 column2;',
+                ],
+                'note' => 'Can be followed by [FIRST|AFTER column_name]',
+                'link' => 'https://www.mysqltutorial.org/mysql-alter-table.aspx'
+            ],
+            [
+                'name' => 'Change table 3',
+                'content' => 'Add column "column1" of table "test"...',
+                'answers' => [
+                    'ALTER TABLE test ADD COLUMN column1',
+                ],
+                'note' => 'Can be followed by [FIRST|AFTER column_name]',
+                'link' => 'https://www.mysqltutorial.org/mysql-alter-table.aspx'
+            ],
+            [
+                'name' => 'Change table 4',
+                'content' => 'Delete column "column1" of table "test".',
+                'answers' => [
+                    'ALTER TABLE test DROP COLUMN column1;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-alter-table.aspx'
+            ],
+            [
+                'name' => 'Grouping',
+                'content' => 'Count unique values of "column1" of table "test".',
+                'answers' => [
+                    'SELECT COUNT(DISTINCT column1) FROM test GROUP BY column1;',
+                ],
+                'note' => '',
+                'link' => 'https://www.mysqltutorial.org/mysql-group-by.aspx'
             ]
         ]
     ];
